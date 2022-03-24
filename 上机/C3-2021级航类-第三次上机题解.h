38e5131b598bd376c9e19d2dff910b2321005adf
Pansis <1449724101@qq.com>
2022-03-25 00:04:30 +0800

    2022.3.23

---
+++ 上机/C3-2021级航类-第三次上机题解.h
@@ -0,0 +1,25 @@
+/*
+ * https://accoding.buaa.edu.cn/contest-ng/index.html#/820
+ C3-2021级航类-第三次上机题解
+ A Ct     //数值范围判断
+ B 求Fibo数列的第n项    //注意中间取模，取模与加减乘除顺序可交换
+ C 三⻆形形状判断
+ D LJF坐地铁
+ E 胆密欧&傅⾥叶
+ F 普通的GCD和LCM
+ G X同学的⽇期统计
+ H zhn の 数论（⼀）
+ I 图图玩狼⼈杀
+ J LJF算复数
+ K cxccxc与谢尔宾斯基三⻆形
+
+取模与加减乘除顺序可交换
+ 对非负整数 a,b,c 与整数 p，模意义下的运算具有如下性质
+(a+b) mod p = [(a mod p)+(b mod p)] mod p
+ (a×b) mod p = [(a mod p)×(b mod p)] mod p
+同时，加法的交换律、结合律，乘法的交换律、结合律、分配律在模意义下仍然成立。
+
+
+
+
+ */
